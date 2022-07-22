0af1131553f5723978380e977c9bd28605d4d65c
jsfix.live analysis <info@coana.tech>
2022-07-22 10:24:05 +0200

    JSFIX patches

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ async function initConnection(uri, options) {
     };
     options = options ? Object.assign(defaultOptions, options) : defaultOptions;
     try {
-        let connection = await mongoose.createConnection(uri, options);
+        let connection = await /* TODO: JSFIX could not patch the breaking change:
+        BREAKING CHANGE: mongoose.connect() returns a promise, removed MongooseThenable #5796
+        Suggested fix: Only relevant if you depend on the return value being a reference to the mongoose object. In that case, you need to modify the usages of the return value to get the mongoose object from somewhere else.*/
+        mongoose.createConnection(uri, options);
         console.log('\x1b[32m%s\x1b[0m', `mongoose.createConnection ${uri} Success`);
         return connection;
     } catch (err) {
